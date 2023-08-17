e27fca422e1f0123786c6c59e6952e09e1614636
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-17 16:37:58 +0300

    corrected syntax mistake

--- src/main/java/by/itclass/model/db/DbInMemory.java
+++ src/main/java/by/itclass/model/db/DbInMemory.java
@@ -11,7 +11,7 @@ public class DbInMemory {
     static {
         countries.add(new Country(1, "USA", "Washington", "Joe Biden", 340000000));
         countries.add(new Country(1, "Poland", "Warsaw", "Andrjey Duda", 60000000));
-        countries.add(new Country(1, "Kazakhstan", "Astana", "Kasyn-Jomart Toqaev", 19000000));
+        countries.add(new Country(1, "Kazakhstan", "Astana", "Kasym-Jomart Toqaev", 19000000));
         countries.add(new Country(1, "France", "Paris", "Emmanuel Macrone", 82000000));
     }
 
