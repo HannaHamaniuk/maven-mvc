629246f750d083cdaad7154ae4780b2e03e1959e
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-18 15:13:18 +0300

    created CountryService class to work with requests

---
+++ src/main/java/by/itclass/model/services/CountryService.java
@@ -0,0 +1,35 @@
+package by.itclass.model.services;
+
+import by.itclass.model.db.DbInMemory;
+import by.itclass.model.entities.Country;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class CountryService {
+    public List<Country> getCountriesByCriteria(Map<String,String[]>params){
+        List<Country>countries = new ArrayList<>();
+        String criteria = params.get("criteria")[0];//достаем первый параметр из мапы критерии
+        switch(criteria){
+            case "name":{
+                String name = params.get("name")[0];
+                countries.add(DbInMemory.findCountryByName(name));
+                countries = countries.stream().filter(Objects::nonNull).collect(Collectors.toList());
+                break;
+            }
+            case"id":{
+                String fromId = params.get("fromId")[0];
+                String toId = params.get("toId")[0];
+                int from = Integer.parseInt(fromId);
+                int to = Integer.parseInt(toId);
+                countries= DbInMemory.findCountriesById(from,to);
+                break;
+            }
+            default:break;
+        }
+        return countries;
+    }
+}
