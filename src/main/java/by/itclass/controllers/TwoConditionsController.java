05d76b77de55403b010b0ba5344dc5be1d7e1af8
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-18 15:39:09 +0300

    added WebServlet annotation

--- src/main/java/by/itclass/controllers/TwoConditionsController.java
+++ src/main/java/by/itclass/controllers/TwoConditionsController.java
@@ -5,6 +5,7 @@ import by.itclass.model.services.CountryService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+@WebServlet(name="twoConditionControllers",urlPatterns = "/criteria")
 public class TwoConditionsController extends HttpServlet {
     private CountryService countryService;
 
