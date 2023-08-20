1f202806c3b1acda883d9324e6a42b80a2546303
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-20 12:16:01 +0300

    updated ConnectionManager class by adding getConnection() method

--- src/main/java/by/itclass/model/db/ConnectionManager.java
+++ src/main/java/by/itclass/model/db/ConnectionManager.java
@@ -1,5 +1,25 @@
 package by.itclass.model.db;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/country_db";
+    private static Connection connection;
+
+    static{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        if(connection == null || connection.isClosed()){
+            return DriverManager.getConnection(URL,"root",null);
+
+        }
+        return connection;
+    }
 }
