96de28839f7de5b433bd55e90d2b5316345a8a1a
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-20 11:54:00 +0300

    created CountryDao class to work ith database

---
+++ src/main/java/by/itclass/model/dao/CountryDao.java
@@ -0,0 +1,15 @@
+package by.itclass.model.dao;
+
+import by.itclass.model.entities.Country;
+
+import java.util.List;
+
+public class CountryDao {
+    public Country findByName(String name){
+        return null;
+    }
+
+    public List<Country> findCountryByIds(int from, int to){
+        return null;
+    }
+}
