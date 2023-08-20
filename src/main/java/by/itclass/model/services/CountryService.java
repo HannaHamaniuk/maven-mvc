3af6a4f8d402581fb2b0da259b53af384f69e967
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-20 13:26:35 +0300

    updated the realization of methods

--- src/main/java/by/itclass/model/services/CountryService.java
+++ src/main/java/by/itclass/model/services/CountryService.java
@@ -1,5 +1,6 @@
 package by.itclass.model.services;
 
+import by.itclass.model.dao.CountryDao;
 import by.itclass.model.db.DbInMemory;
 import by.itclass.model.entities.Country;
 
@@ -10,13 +11,20 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CountryService {
+    private CountryDao countryDao;
+
+    public CountryService() {
+        countryDao = new CountryDao();
+    }
+
     public List<Country> getCountriesByCriteria(Map<String,String[]>params){
         List<Country>countries = new ArrayList<>();
         String criteria = params.get("criteria")[0];//достаем первый параметр из мапы критерии
         switch(criteria){
             case "name":{
                 String name = params.get("name")[0];
-                countries.add(DbInMemory.findCountryByName(name));
+                //countries.add(DbInMemory.findCountryByName(name));
+                countries.add(countryDao.findByName(name));
                 countries = countries.stream().filter(Objects::nonNull).collect(Collectors.toList());
                 break;
             }
@@ -25,7 +33,8 @@ public class CountryService {
                 String toId = params.get("toId")[0];
                 int from = Integer.parseInt(fromId);
                 int to = Integer.parseInt(toId);
-                countries= DbInMemory.findCountriesById(from,to);
+                //countries= DbInMemory.findCountriesById(from,to);
+                countries = countryDao.findCountryByIds(from,to);
                 break;
             }
             default:break;
