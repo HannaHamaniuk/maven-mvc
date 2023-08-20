9f345aac03010fcebdd6be0cfb4a7cb2801b11d1
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-08-20 12:46:55 +0300

    updated CountryDao class with methods findCountryByName() and findCountrybyIds()

--- src/main/java/by/itclass/model/dao/CountryDao.java
+++ src/main/java/by/itclass/model/dao/CountryDao.java
@@ -1,15 +1,62 @@
 package by.itclass.model.dao;
 
+import by.itclass.model.db.ConnectionManager;
 import by.itclass.model.entities.Country;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CountryDao {
-    public Country findByName(String name){
+    private static final String QUERY_NAME = "SELECT id, name, capital, president,population FROM country WHERE name LIKE ?";
+    private static final String QUERY_ID = "SELECT id, name, capital, president,population FROM country WHERE id>=? AND id<=?";
+
+    public Country findByName(String name) {
+        try (Connection cn = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = cn.prepareStatement(QUERY_NAME)) {
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String capital = resultSet.getString("capital");
+                String president = resultSet.getString("president");
+                int population = resultSet.getInt("population");
+                return new Country(id, name, capital, president, population);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
+
     }
 
-    public List<Country> findCountryByIds(int from, int to){
-        return null;
+    public List<Country> findCountryByIds(int from, int to) {
+        List<Country> countries = new ArrayList<>();
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement ps = connection.prepareStatement(QUERY_ID)) {
+            ps.setInt(1, from);
+            ps.setInt(2, to);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String capital = rs.getString("capital");
+                String president = rs.getString("president");
+                int population = rs.getInt("population");
+                countries.add(new Country(id, name, capital, president, population));
+
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return countries;
     }
 }
